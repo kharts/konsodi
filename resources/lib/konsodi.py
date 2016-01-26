1895862241b1cf35c9c919bca8af032bc8f3cb2a
kharts <kharts@ukr.net>
2016-01-25 20:30:51 -0800

    Set geometry of the main window

--- resources/lib/konsodi.py
+++ resources/lib/konsodi.py
@@ -9,6 +9,12 @@
 import pyxbmct
 
 
+WINDOW_WIDTH = 640
+WINDOW_HEIGHT = 480
+WINDOW_ROWS = 1
+WINDOW_COLUMNS = 1
+
+
 def start():
     """
     Start the plugin
@@ -26,3 +32,9 @@ class MainWindow(pyxbmct.AddonDialogWindow):
 
     def __init__(self, title):
         super(MainWindow, self).__init__(title)
+        self.setGeometry(
+            WINDOW_WIDTH,
+            WINDOW_HEIGHT,
+            WINDOW_ROWS,
+            WINDOW_COLUMNS
+        )
