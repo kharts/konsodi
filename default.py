aa65d214778e1f3d9cbb1c3b80abb685432f17bc
kharts <kharts@ukr.net>
2016-01-27 21:56:02 -0800

    Rename plugin.py to default.py

---
+++ default.py
@@ -0,0 +1,10 @@
+# default.py - starting point of the addon
+#
+# Copyright 2016 kharts (https://github.com/kharts)
+#
+# This software may be used and distributed according to the terms of the
+# GNU General Public License version 2 or any later version.
+
+from resources.lib import konsodi
+
+konsodi.start()
